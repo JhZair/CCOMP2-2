e8bb9ad22ea7c56e1db9d5449eae3d6996dd4116
JhZair <98540197+JhZair@users.noreply.github.com>
2024-09-18 08:08:49 -0500

    Update Problema1.cpp

--- Ejercicios-práctica/Problema1.cpp
+++ Ejercicios-práctica/Problema1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
     int b=0;
-    for (int x=1;x<10000;x++){
+    for (int x=1;x<1000000;x++){
         if (x % 5 == 0|| x % 3 == 0){
             //cout << x <<" ,";
             b += x;
