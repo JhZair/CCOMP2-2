76b84cf3b47991865a874b69a9de18ce81bf451d
JhZair <98540197+JhZair@users.noreply.github.com>
2024-09-18 07:31:05 -0500

    Update Problema3.cpp

--- Ejercicios-práctica/Problema3.cpp
+++ Ejercicios-práctica/Problema3.cpp
@@ -1,70 +1,21 @@
-/*
 #include <iostream>
 
 using namespace std;
-bool esPrimo(int a){
-    int b = 0;
-    int x = a;
-    for (; x>0; x--){
-        if (x % a == 0)
-            b++;
-    }
-    if (b>2){
-        cout << "Es primo";    
-        return true;
-    }
-    else
-        return false;    
-}
-int main(){
-    cout << "Ingresa un numero: ";
-    int a = 0;
-    cin >> a;
-    for (;a > 0;a--){
-        if (esPrimo(a)) {
-            cout << a << endl;
-        }
-        else{
-            cout << "No es primo"<<endl;
-        }
-    }
-    return 0;
-}   
-*/
 
-#include <iostream>
-
-bool esPrimo(int num) {
-    if (num <= 1) {
-        return false;  // Los números menores o iguales a 1 no son primos
+bool esPrimo(int x){
+    for(int i=2;i<x;i++){
+        if(x%i == 0)
+            return false;
     }
-    
-    for (int i = 2; i * i <= num; ++i) {
-        if (num % i == 0) {
-            return false;  // Si es divisible por cualquier número, no es primo
-        }
-    }
-    
-    return true;  // Si no tiene divisores, es primo
+    return true;
 }
 
 int main() {
-    int n;
-    
-    // Solicitar el número
-    std::cout << "Introduce un número: ";
-    std::cin >> n;
-
-    // Imprimir todos los números primos menores a n
-    std::cout << "Números primos menores a " << n << ": ";
-    
-    for (int i = 2; i < n; ++i) {
-        if (esPrimo(i)) {
-            std::cout << i << " ";
-        }
+    int n = 0;
+    cout << "Ingresa un número: ";
+    cin >> n;
+    for(int i=2;i<n;i++){
+        if (esPrimo(i))
+            cout << i << endl;
     }
-    
-    std::cout << std::endl;
-    
-    return 0;
 }
