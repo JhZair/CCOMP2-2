0df7d07798d1178e99e03da5b8871377112a99fe
JohnZair <johnzairoros@gmail.com>
2024-10-03 21:19:37 -0500

    Archivos borrados

--- Archivos de clase/helloWorld.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-// Tu primer programa en C++
-int main()
-{
-        cout << "Hola mundin" << endl;
-        return 0;
-}
--- Capitulo-2/2.21.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-
-   std::cout << "  CCC\t+\t+\n C\t+\t+\nC     +++++   +++++\n C\t+\t+\n  CCC\t+\t+"
-      << std::endl;
-
-   return 0;
-}
